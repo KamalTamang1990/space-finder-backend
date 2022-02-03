fbce531449ff93f8bb7298cdbd3e72ee38c7c940
Inctic <ktamang@inctic.io>
2022-02-03 13:17:24 +0545

    added put items in dynamodb with apigateway

--- services/SpacesTable/Create.ts
+++ services/SpacesTable/Create.ts
@@ -4,6 +4,7 @@ import { DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { v4 } from 'uuid';
 
+
 // this is lambda it is best for us to use the imports from Lambda
 // need some types from aws [npm i @types/aws-lambda]
 // now we will use the lambda with API Gateway
@@ -16,15 +17,22 @@ async function handler(event: APIGatewayProxyEvent, context: Context): Promise<A
         statusCode: 200,
         body: 'Hello from Dynamodb'
     }
+
+
+    // we can get data from APIGatewayPRoxyEvent and put it into our dynamodb table
+    const item = typeof event.body == 'object'? event.body: JSON.parse(event.body);
+    item.spaceId = v4();
+
     // this is the iteam that will be insterted into our dynamodb
-    const item = {
+    /*const item = {
         // need to be unique so it has [uuid]
         spaceId: v4()
     
-    }
+    }*/
+
     //action from dynamodb
     try {
-        // the action is put of PutItemInput
+        // the action is PutItemInput
         await dbClient.put({
             // Tablename from spacestack
             TableName: 'SpacesTable',
@@ -33,6 +41,9 @@ async function handler(event: APIGatewayProxyEvent, context: Context): Promise<A
     } catch (error) {
         result.body = (error as Error).message
     }
+
+    result.body = JSON.stringify(item.spaceId)
+    //result.body = JSON.stringify('Created item with id: ${item.spaceId}')
     return result
 }
 export { handler }
